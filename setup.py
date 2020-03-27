10b5578079d82e7c77e8c15b0e93d72b88e6ddfe
marlinmm <marlinmueller.mm@gmail.com>
2020-03-27 13:09:35 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+directory = os.path.abspath(os.path.dirname(__file__))
+if sys.version_info >= (3, 0):
+    with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(directory, 'README.md')) as f:
+        long_description = f.read()
+
+setup(name='BanDiTS',
+      packages=find_packages(),
+      include_package_data=True,
+      setup_requires=['setuptools_scm'],
+      use_scm_version=True,
+      description='A Python module for time series analysis',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+      ],
+      install_requires=["pyroSAR"],
+      python_requires='>=3.6.0',
+      url='https://github.com/marlinmm/GEO450_GRASS_S1.git',
+      author='', #'Jonas Ziemer', Marlin Mueller, Patrick Fischer  # how to add second author?
+      author_email='',
+      license='MIT',
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown')
