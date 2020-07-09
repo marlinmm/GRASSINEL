1a7eedeb785e1cf36c8c211206ba121c6e63d5c0
marlinmm <marlinmueller.mm@gmail.com>
2020-07-09 17:56:37 +0200

    cleaned up file and made differentiation between windows and linux GRASSBIN work, still needs work with paths and location naming for GRASS GISS stuff

--- GEO450_GRASS_S1/python_setup.py
+++ GEO450_GRASS_S1/python_setup.py
@@ -6,7 +6,19 @@ sys.path.insert(0, os.path.join(os.environ['GISBASE'], 'etc', 'python'))
 
 ##################################################################################
 # general GRASS setup
-grassbin = "grass79"
+# Windows
+grass7bin_win = r'C:/OSGeo4W64/bin/grass79.bat'
+# Linux
+grass7bin_lin = 'grass79'
+
+if sys.platform.startswith('linux'):
+    # we assume that the GRASS GIS start script is available and in the PATH
+    # query GRASS 7 itself for its GISBASE
+    grass7bin = grass7bin_lin
+elif sys.platform.startswith('win'):
+    grass7bin = grass7bin_win
+
+grassbin = grass7bin
 os.environ['GRASSBIN'] = grassbin
 
 from grass_session import Session, get_grass_gisbase
@@ -25,21 +37,25 @@ os.environ['PROJ_LIB'] = '/usr/share/proj'
 # to add grass to path permanently, open "bashrc" using command: vim bash
 import grass.script as gscript
 import grass.script.setup as gsetup
-
 ##################################################################################
 # user-defined settings
-gisdb = '/home/user/grassdata'
-location = 'test1'
+### Linux path <- add path here for each Linux user and do not overwrite!
+#gisdb = '/home/user/grassdata'
+
+### Windows path <- add path here for each Windows user and do not overwrite!
+gisdb = 'F:/GEO450_GRASS/test_python'
+location = 'test3'
 mapset = 'PERMANENT'
 ##################################################################################
 # open a GRASS session and create the mapset if it does not yet exist
 with Session(gisdb=gisdb,
              location=location,
-             create_opts='EPSG:32629') as session:
+             mapset="user4",
+             create_opts='EPSG:25832') as session:
     pass
 ##################################################################################
 # launch session
 gsetup.init(gisbase, gisdb, location, mapset)
-print('Current GRASS GIS 7 environment:')
-print(gscript.gisenv())
+# nprint('Current GRASS GIS 7 environment:')
+# print(gscript.gisenv())
 ##################################################################################
