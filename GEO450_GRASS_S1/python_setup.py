2ca3fdac652a286daa1b327c61c582d8d3986fe8
marlinmm <marlinmueller.mm@gmail.com>
2020-07-09 17:00:05 +0300

    corrected paths and stuff

--- GEO450_GRASS_S1/python_setup.py
+++ GEO450_GRASS_S1/python_setup.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.join(os.environ['GISBASE'], 'etc', 'python'))
 
 ##################################################################################
 # general GRASS setup
-grassbin = 'grass79'
+grassbin = "grass79"
 os.environ['GRASSBIN'] = grassbin
 
 from grass_session import Session, get_grass_gisbase
@@ -19,15 +19,17 @@ sys.path.append(os.path.join(os.environ['GISBASE'], 'lib'))
 sys.path.append(os.path.join(os.environ['GISBASE'], 'scripts'))
 sys.path.append(os.path.join(os.environ['GISBASE'], 'etc', 'python'))
 
-os.environ['PROJ_LIB'] = 'C:\\OSGeo4W64\\share\\proj'
+# set folder to proj_lib:
+os.environ['PROJ_LIB'] = '/usr/share/proj'
 
+# to add grass to path permanently, open "bashrc" using command: vim bash
 import grass.script as gscript
 import grass.script.setup as gsetup
 
 ##################################################################################
 # user-defined settings
-gisdb = 'E:\\DATA\\test\\GRASS'
-location = 'Spain_Donana'
+gisdb = '/home/user/grassdata'
+location = 'test1'
 mapset = 'PERMANENT'
 ##################################################################################
 # open a GRASS session and create the mapset if it does not yet exist
