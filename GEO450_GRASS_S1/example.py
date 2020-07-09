149a8c44cbd5d9f0050dd5f802ab9ccb1c2ac683
marlinmm <marlinmueller.mm@gmail.com>
2020-07-09 20:31:11 +0300

    corrected som errors, kinda works now under linux

--- GEO450_GRASS_S1/example.py
+++ GEO450_GRASS_S1/example.py
@@ -23,11 +23,11 @@ from grass.pygrass.modules.shortcuts import display as d
 
 # simple example for pyGRASS usage: raster processing via modules approach
 # Windows path
-gisdb = 'F:/GEO450_GRASS/test_python'
+# gisdb = 'F:/GEO450_GRASS/test_python'
 # Linux path
-# gisdb = '/home/user/grassdata'
-location = 'test1'
-mapset = 'user'
+gisdb = '/home/user/grassdata'
+location = 'test3'
+mapset = 'PERMANENT'
 
 ## IMPORT AND INSTALL SENTINELSAT FIRST !!! ##
 
@@ -35,20 +35,23 @@ mapset = 'user'
 with Session(gisdb=gisdb, location=location, create_opts='EPSG:32632'):
     #def ogrimport():
     ## function body to be added here ##
+
+
+    #### RUN THIS SHIT BEFORE RUNNING SENTINELDOWNLOAD!!!!!!
     # ogrimport = Module("v.in.ogr")
-    # ogrimport("/home/user/Desktop/GRASS Jena Workshop/geodata/osm/jena_rivers.gpkg")
-    # print(gcore.run_command())
-    # print(v.info(map='jena_rivers'))
+    # ogrimport("/home/user/Desktop/GRASS Jena Workshop/geodata/osm/jena_boundary.gpkg")
+
+    #print(v.info(map='jena_boundary'))
 
     sentineldownload = Module("i.sentinel.download")
     sentineldownload(
                 ### Linux folder ###
-                # settings="/home/user/Desktop/GRASS Jena Workshop/settings.txt",
-                # output="/home/user/Desktop/GRASS Jena Workshop/geodata/sentinel/Sentinel_Download",
-                ### Windows folder ###
                 settings="/home/user/Desktop/GRASS Jena Workshop/settings.txt",
-                output="F:/GEO450_GRASS/Data/sentinel/test_GEO450",
-                map="test1@user",
+                output="/home/user/Desktop/GRASS Jena Workshop/geodata/sentinel/Sentinel_Download",
+                ### Windows folder ###
+                # settings="/home/user/Desktop/GRASS Jena Workshop/settings.txt",
+                # output="F:/GEO450_GRASS/Data/sentinel/test_GEO450",
+                map="jena_boundary@PERMANENT",
                 area_relation="Intersects",
                 producttype="GRD",
                 start="2020-06-02",
