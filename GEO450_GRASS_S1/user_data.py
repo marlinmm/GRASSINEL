f9e6e23b1ff273e9d9d1d614e9bb177f6088635d
marlinmm <marlinmueller.mm@gmail.com>
2020-07-21 14:35:53 +0300

    added new class and more userdata

--- GEO450_GRASS_S1/user_data.py
+++ GEO450_GRASS_S1/user_data.py
@@ -1,6 +1,11 @@
 import os
-######## add user directory ########
 
+######## add user data for grass setup ########
+location_name = "GEO450_test1"
+crs = "32632"
+grass_version = "grass79"
+
+######## add user directory ########
 main_dir = os.path.abspath("/home/user/Desktop/GEO450_main_dir")
 
 ######## create needed directories ########
@@ -15,6 +20,13 @@ if not os.path.exists(sen_down_dir):
     os.makedirs(sen_down_dir)
 if not os.path.exists(sen_processed_dir):
     os.makedirs(sen_processed_dir)
+
+
+class GRASS_data(object):
+    location_name = location_name
+    crs = crs
+    grass_version = grass_version
+
 
 class Paths(object):
     grass_path = grass_dir
