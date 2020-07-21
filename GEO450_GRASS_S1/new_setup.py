a50cfa57ede10e97226f5eaac2af739cf34fab5e
marlinmm <marlinmueller.mm@gmail.com>
2020-07-21 13:10:15 +0300

    added new_setup.py for creating Grass setup

---
+++ GEO450_GRASS_S1/new_setup.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from grass_session import Session, get_grass_gisbase
+from GEO450_GRASS_S1.main_test import Paths
+from grass_session import Session
+import sentinelsat
+from grass.script import core as gcore
+import grass.script as gscript
+import grass.script.setup as gsetup
+# import grass python libraries
+from grass.pygrass.modules import Module
+from grass.pygrass.modules.shortcuts import general as g
+from grass.pygrass.modules.shortcuts import raster as r
+from grass.pygrass.modules.shortcuts import vector as v
+from grass.pygrass.modules.shortcuts import temporal as t
+from grass.pygrass.modules.shortcuts import display as d
+from config import Grass
+
+
+def GRASSBIN_import(version):
+    # general GRASS setup
+    # input your Windows path
+    grass7bin_win = r'C:/OSGeo4W64/bin/grass79.bat'
+    # set your Linux grass version
+    grass7bin_lin = version
+
+    if sys.platform.startswith('linux'):
+        # we assume that the GRASS GIS start script is available and in the PATH
+        # query GRASS 7 itself for its GISBASE
+        grass7bin = grass7bin_lin
+    elif sys.platform.startswith('win'):
+        grass7bin = grass7bin_win
+    print(grass7bin)
+    return grass7bin
+
+
+def grass_setup(location_name, crs, grass_version):
+    """
+    ....
+    """
+    grassbin = GRASSBIN_import(version=grass_version)
+    os.environ['GRASSBIN'] = grassbin
+    gisbase = get_grass_gisbase()
+    os.environ['GISBASE'] = gisbase
+    sys.path.append(os.path.join(os.environ['GISBASE'], 'bin'))
+    sys.path.append(os.path.join(os.environ['GISBASE'], 'lib'))
+    sys.path.append(os.path.join(os.environ['GISBASE'], 'scripts'))
+    sys.path.append(os.path.join(os.environ['GISBASE'], 'etc', 'python'))
+
+    # set folder to proj_lib:
+    os.environ['PROJ_LIB'] = '/usr/share/proj'
+
+    gisdb = Paths.grass_path
+    mapset = "PERMANENT"
+    ##################################################################################
+    # open a GRASS session and create the mapset if it does not yet exist
+    with Session(gisdb=gisdb,
+                 location=location_name,
+                 create_opts='EPSG:' + crs) as session:
+        pass
+    ##################################################################################
+    # launch session
+    gsetup.init(gisbase, gisdb, location_name, mapset)
+    print(f"Current GRASS GIS 7 environment: {gscript.gisenv()}")
