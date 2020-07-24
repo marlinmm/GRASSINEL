cfdd49234f4dff5a75aa47823f7c9d9c2b937d48
marlinmm <marlinmueller.mm@gmail.com>
2020-07-24 15:27:04 +0300

    added timing code to main func

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -1,4 +1,6 @@
 from GEO450_GRASS_S1.grass_functionality import *
+from datetime import datetime
+
 
 def main():
     grass_setup()
@@ -9,4 +11,7 @@ def main():
 
 
 if __name__ == "__main__":
+    start_time = datetime.now()
     main()
+    end_time = datetime.now()
+    print("extract_files-time = ", end_time - start_time, "Hr:min:sec")
