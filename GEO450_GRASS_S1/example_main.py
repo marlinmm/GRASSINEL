927eb59aa19541cb7e890fbb4d12d3d9486fcf32
marlinmm <marlinmueller.mm@gmail.com>
2020-07-25 20:57:44 +0300

    changed how the main processing functions are calles

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -4,10 +4,10 @@ from datetime import datetime
 
 def main():
     grass_setup()
-    #import_shapefile(overwrite=True)
-    #sen_download(start_time="2020-06-01", end_time="2020-06-02", sort_by="ingestiondate")
-    #test()
-    pyroSAR_processing()
+    # import_shapefile(path_to_shape=Paths.boundary_path, overwrite_bool=True)
+    # sen_download(start_time="2020-05-28", end_time="2020-06-06", sort_by="ingestiondate")
+    # test()
+    pyroSAR_processing(target_resolution=50, target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=False)
 
 
 if __name__ == "__main__":
