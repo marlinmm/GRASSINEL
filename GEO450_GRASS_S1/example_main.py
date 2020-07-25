d74ecc2cd756d7508eb308be0f8eecf7b3b7063d
marlinmm <marlinmueller.mm@gmail.com>
2020-07-26 00:35:30 +0300

    added time statement and widend start and end time for use with singular "relative_orbit_number" parameter

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -5,9 +5,9 @@ from datetime import datetime
 def main():
     grass_setup()
     # import_shapefile(path_to_shape=Paths.boundary_path, overwrite_bool=True)
-    # sen_download(start_time="2020-05-28", end_time="2020-06-06", sort_by="ingestiondate")
+    # sen_download(start_time="2020-05-01", end_time="2020-05-15", sort_by="ingestiondate")
     # test()
-    pyroSAR_processing(target_resolution=50, target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=False)
+    pyroSAR_processing(start_time=start_time, target_resolution=50, target_CRS=32632, terrain_flat_bool=True, remove_therm_noise_bool=True)
 
 
 if __name__ == "__main__":
