21f810fa77d099927b80646377e3368387757921
marlinmm <marlinmueller.mm@gmail.com>
2020-07-26 14:27:54 +0300

    changed some minor stuff

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -6,9 +6,9 @@ def main():
     grass_setup()
     # import_shapefile(path_to_shape=Paths.boundary_path, overwrite_bool=True)
     # sen_download(start_time="2020-05-01", end_time="2020-05-04", sort_by="ingestiondate")
-    sen_download_new(start_time="2020-05-01", end_time="2020-05-30", sort_by="ingestiondate", relative_orbit_number=168)
+    # sen_download_new(start_time="2020-05-01", end_time="2020-05-30", sort_by="ingestiondate", relative_orbit_number=168)
     # test()
-    pyroSAR_processing(start_time=start_time, target_resolution=50, target_CRS=32632, terrain_flat_bool=True, remove_therm_noise_bool=True)
+    pyroSAR_processing(start_time=start_time, target_resolution=50, target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=False)
 
 
 if __name__ == "__main__":
