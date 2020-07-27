9d7b48aeac18c2ace70819c23d965013ea4da084
jziemer1996 <jz199623@gmail.com>
2020-07-27 19:23:49 +0300

    added todo statement and changed "processing time"

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -1,8 +1,7 @@
 from GEO450_GRASS_S1.grass_functionality import *
 from datetime import datetime
 
-
-
+# TODO: DEFINE USER DEPENDENT INPUT PATHS AND FILES -> HERE or in USER_DATA.py !?!?!?
 
 def main():
     grass_setup()
@@ -10,13 +9,13 @@ def main():
     # sen_download(start_time="2020-05-01", end_time="2020-05-04", sort_by="ingestiondate")
     # sen_download_new(start_time="2020-05-01", end_time="2020-05-30", sort_by="ingestiondate", relative_orbit_number=168)
     # pyroSAR_processing(start_time=start_time, target_resolution=50, target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=False)
-    subset_import(subset_path="/media/user/2nd_disk/sen_processed_dir/subset",
-                  filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelist.txt", overwrite_bool=True)
-    create_stc(overwrite_bool=True, filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelist.txt")
+    #subset_import(subset_path="/media/user/2nd_disk/sen_processed_dir/subset",
+    #              filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelistVH.txt", overwrite_bool=True, output="rasterVH", polarization_type="VH")
+    create_stc(overwrite_bool=True, filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelistVV.txt", output="stcVV")
 
 
 if __name__ == "__main__":
     start_time = datetime.now()
     main()
     end_time = datetime.now()
-    print("extract_files-time = ", end_time - start_time, "Hr:min:sec")
+    print("processing-time = ", end_time - start_time, "Hr:min:sec")
