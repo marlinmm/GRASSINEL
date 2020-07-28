60fcfa99ad652ba3e5bbdbc5e41eb0a23c248985
marlinmm <marlinmueller.mm@gmail.com>
2020-07-28 19:32:13 +0300

    changed way functions are fed with variables

--- GEO450_GRASS_S1/example_main.py
+++ GEO450_GRASS_S1/example_main.py
@@ -9,9 +9,8 @@ def main():
     # sen_download(start_time="2020-05-01", end_time="2020-05-04", sort_by="ingestiondate")
     # sen_download_new(start_time="2020-05-01", end_time="2020-05-30", sort_by="ingestiondate", relative_orbit_number=168)
     # pyroSAR_processing(start_time=start_time, target_resolution=50, target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=False)
-    #subset_import(subset_path="/media/user/2nd_disk/sen_processed_dir/subset",
-    #              filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelistVV.txt", overwrite_bool=True, output="rasterVV", polarization_type="VV")
-    create_stc(overwrite_bool=True, filelist_path="/home/user/Desktop/GRASS Jena Workshop/sentinel-filelistVH.txt", output="stcVH")
+    subset_import(subset_path=Paths.subset_path, overwrite_bool=True, output="raster", polarization_type="VH")
+    create_stc(overwrite_bool=True, output="stc", polarization_type="VH")
     #t_rast_algebra(basename="product1", expression="result1 =(stcVH*stcVV)/stcVH")
 
 
