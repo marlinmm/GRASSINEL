139a0ec4e136b1947995667cc328d6fc93a19790
marlinmm <marlinmueller.mm@gmail.com>
2020-07-31 17:13:19 +0300

    changed path names, created the user_data function and changed some stuff in the path classes

--- GRASSINEL/user_data.py
+++ GRASSINEL/user_data.py
@@ -1,39 +1,36 @@
 import os
 
-######## add user data for grass setup ########
-location_name = "GEO450_test1"
-crs = "32632"
-grass_version = "grass79"
 
-######## add user directory ########
-boundary_shape = "/home/user/Desktop/GRASS Jena Workshop/geodata/osm/jena_boundary.gpkg"
-main_dir = os.path.abspath("/home/user/Desktop/GEO450_main_dir")
-data_dir = os.path.abspath("/media/user/2nd_disk")
-######## create needed directories ########
+def user_data():
+    ######## add user data for grass setup ########
+    location_name = "GRASSINEL_test1"
+    crs = "32632"
+    grass_version = "grass79"
 
-grass_dir = os.path.join(main_dir, 'grass_dir')
-sen_down_dir = os.path.join(data_dir, 'sen_down_dir')
-sen_processed_dir = os.path.join(data_dir, 'sen_processed_dir')
-subset_dir = os.path.join(sen_processed_dir, "subset")
+    ######## add user directory ########
+    boundary_shape = "/home/user/Desktop/GRASS Jena Workshop/geodata/osm/jena_boundary.gpkg"
+    main_dir = os.path.abspath("/home/user/Desktop/GRASSINEL_dir")
+    data_dir = os.path.abspath("/media/user/2nd_disk")
+    ######## create needed directories ########
 
-if not os.path.exists(grass_dir):
-    os.makedirs(grass_dir)
-if not os.path.exists(sen_down_dir):
-    os.makedirs(sen_down_dir)
-if not os.path.exists(sen_processed_dir):
-    os.makedirs(sen_processed_dir)
+    grass_dir = os.path.join(main_dir, 'grass_dir')
+    sen_down_dir = os.path.join(data_dir, 'sen_down_dir')
+    sen_processed_dir = os.path.join(data_dir, 'sen_processed_dir')
+    subset_dir = os.path.join(sen_processed_dir, "subset")
+
+    if not os.path.exists(grass_dir):
+        os.makedirs(grass_dir)
+    if not os.path.exists(sen_down_dir):
+        os.makedirs(sen_down_dir)
+    if not os.path.exists(sen_processed_dir):
+        os.makedirs(sen_processed_dir)
+
+    return location_name, crs, grass_version, main_dir, grass_dir, sen_down_dir, sen_processed_dir, subset_dir,\
+           boundary_shape
 
 
 class GRASS_data(object):
-    location_name = location_name
-    crs = crs
-    grass_version = grass_version
-
+    location_name, crs, grass_version = user_data()[0:3]
 
 class Paths(object):
-    main_path = main_dir
-    grass_path = grass_dir
-    send_down_path = sen_down_dir
-    sen_processed_path = sen_processed_dir
-    subset_path = subset_dir
-    boundary_path = boundary_shape
+    main_path, grass_path, sen_down_path, sen_processed_path, subset_path, boundary_path = user_data()[3:10]
