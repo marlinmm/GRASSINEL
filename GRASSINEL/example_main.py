636d40b9a5e2d69a7afc4bb2eb2e57d110ba0ba0
jziemer1996 <jz199623@gmail.com>
2020-08-14 18:11:44 +0300

    updated main

--- GRASSINEL/example_main.py
+++ GRASSINEL/example_main.py
@@ -11,19 +11,19 @@ def main():
 
     # ######## SENTINEL DOWNLOAD FUNCTION(S) #############
     # sen_download(start_time="2020-05-01", end_time="2020-05-04", sort_by="ingestiondate")
-    sen_download_extended(start_time="2020-05-01", end_time="2020-06-05", sort_by="ingestiondate",
-                          relative_orbit_number=168)
+    # sen_download_extended(start_time="2020-05-01", end_time="2020-06-05", sort_by="ingestiondate",
+    #                       relative_orbit_number=168)
 
     # ########## SENTINEL PREPROCESSING FUNCTION(S) #############
     # !!!!! terrain_flat_bool needs to be set to False, or VV_data don't contain any information !!!!! #
     # pyroSAR_processing(down_path=Paths.sen_down_path, processed_path=Paths.sen_processed_path, target_resolution=50,
     #                    target_CRS=32632, terrain_flat_bool=False, remove_therm_noise_bool=True)
     # subset_import(overwrite_bool=True, output="raster", polarization_type=["VH", "VV"])
-    #
+
     ########## GRASS SPACE TIME CUBE FUNCTION(S) #############
     create_stc(overwrite_bool=True, output="stcube", polarization_type=["VH", "VV"], stc_info_bool=True,
                stc_statistics_bool=True)
-    visualize_stc(output="stcube", polarization_type=["VH", "VV"], stc_animation_bool=True, stc_timeline_bool=True)
+    # visualize_stc(output="stcube", polarization_type=["VH", "VV"], stc_animation_bool=True, stc_timeline_bool=True)
     # raster_comparison(raster1_name="rasterVH0@PERMANENT", raster2_name="rasterVH1@PERMANENT", mode="swipe")
 
     # ########## GRASS ANALYSIS FUNCTION(S) #############
